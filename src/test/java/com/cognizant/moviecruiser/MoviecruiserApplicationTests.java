f0ee7d6413aad98c7f456697848f24dce247816a
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2020-06-15 19:50:09 +0530

    Update MoviecruiserApplicationTests.java

--- src/test/java/com/cognizant/moviecruiser/MoviecruiserApplicationTests.java
+++ src/test/java/com/cognizant/moviecruiser/MoviecruiserApplicationTests.java
@@ -1,13 +1,13 @@
-package com.cognizant.moviecruiser;
+// package com.cognizant.moviecruiser;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MoviecruiserApplicationTests {
+// @SpringBootTest
+// class MoviecruiserApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
